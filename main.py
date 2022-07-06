ec4aaac81e767141ea60e9433c74d548ff56f55a
sho-engineer <arlonbolt@gmail.com>
2022-07-07 00:24:01 +0900

    add initializing the setting for recording

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import speech_recognition as sr
 from models import Text
+from texts import TEXTS
+import pyaudio
+import wave
+import os
 
 TEXTS = [
     "My father was born in 1927. He was 92 years old when he passed away and had been married to my mother for sixty-three years. Even as my father got older, he was in amazing health. However, this past March, my father was unexpectedly diagnosed with terminal cancer that had begun in his lungs and had spread into his bones. "
