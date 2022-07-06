8c5c6dc6dbcbe692a14ebc3f7e364d63e4dc1fc1
sho-engineer <arlonbolt@gmail.com>
2022-07-06 23:44:36 +0900

    add voice record test

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import speech_recognition as sr
 from models import Text
 
-try:
+TEXTS = [
+    "My father was born in 1927. He was 92 years old when he passed away and had been married to my mother for sixty-three years. Even as my father got older, he was in amazing health. However, this past March, my father was unexpectedly diagnosed with terminal cancer that had begun in his lungs and had spread into his bones. "
+]
+
+def execute_record():
     r = sr.Recognizer()
     with sr.Microphone() as input:
-        print("recording is progress...")
+        print("recording is in progress...")
         audio = r.listen(input)
         text = r.recognize_google(audio, language='en-EN')
         print(text)
-        text_correct = Text("this is what I need to do")
-        if text == text_correct.text:
-            print("correct")
-        else:
-            print("falied")
+    return text
 
-except Exception as e:
-    print(e)
+def parse_text(text):
+    parsed_text = text.split('.')
+    return parsed_text
+
+def upper_to_lower(text):
+    return text.lower()
+
+if __name__ == '__main__':
+    # execute_record()
+    try:
+        for text in parse_text(TEXTS[0]):
+            input_text = execute_record()
+            lower_input_text = upper_to_lower(input_text)
+            text = upper_to_lower(text)
+            print(input_text)
+            print(text)
+
+            if lower_input_text == text:
+                print("correct")
+            else:
+                print('false')
+    except Exception as e:
+        print(f'error occured: {e}')
--- models.py
+++ models.py
@@ -2,5 +2,3 @@ class Text:
     def __init__(self, text):
         self.text = text
         self.score = 0
-
-text = Text("This is what I need to do")
