7938a9ca77fcc1553e7c77f17273201f398f31c1
sho-engineer <arlonbolt@gmail.com>
2022-07-10 00:38:12 +0900

    fix add some comments to the functions to record

--- main.py
+++ main.py
@@ -42,37 +42,47 @@ def init_record_setting():
     # the number of used microphones 
     CHANNELS = 1
     # the time to record(length)
-    record_time = 5
+    record_time = 3
     RATE = 44100
     # get ready to save the record data
     date_today = get_today_date()
-    output_path =  os.path.join("recorded_voices", f'{date_today}_Example_Text')
+    output_path = os.path.join("../recorded_voices", f'{date_today}.wav')
 
     return {'chunk':CHUNK, 'format':FORMAT, 'channel':CHANNELS, 'record_time':record_time, 'rate':RATE, 'output_path':output_path }
 
 def record():
+    # initialize the setting for recording
     init = init_record_setting()
-    py_audio = pyaudio.PyAudio()
-    # create an PuyAudio instance 
     stream = py_audio.open(format=init['format'], channels=init['channel'], rate=init['rate'],
+    py_audio = pyaudio.PyAudio()
+    # open pyaudio stream with the setting defined above
                             input=True, frames_per_buffer=init['chunk'])
     frames = []
+    # define an empty array 
     for i in range(0, int(init['rate'] / init['chunk'] * init['record_time'])):
+    # record body
         data = stream.read(init['chunk'])
         frames.append(data)
     stream.stop_stream()
+    # stop streaming
     stream.close()
     py_audio.terminate()
     output_wave_file(frames=frames, pyaudio=py_audio)
+    # output as a wave file
 
 def output_wave_file(frames, pyaudio):
     init = init_record_setting()
+    # initiaze the setting for recording
     wf = wave.open(init['output_path'], 'wb')
+    # open a file with writable mode
     wf.setnchannels(init['channel'])
+    # set some settings 
     wf.setsampwidth(pyaudio.get_sample_size(init['format']))
     wf.setframerate(init['rate'])
     wf.writeframes(b''.join(frames))
+    # write data to the wave file
     wf.close()
+    # end output
 
 if __name__ == '__main__':
     # execute_record()
