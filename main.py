e9ae3b342df6a4bc57113a00d39d3b1b4c1f1926
sho-engineer <arlonbolt@gmail.com>
2022-07-10 00:22:17 +0900

    fix separate the file to 'texts.py'

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import pyaudio
 import wave
 import os
 
-TEXTS = [
-    "My father was born in 1927. He was 92 years old when he passed away and had been married to my mother for sixty-three years. Even as my father got older, he was in amazing health. However, this past March, my father was unexpectedly diagnosed with terminal cancer that had begun in his lungs and had spread into his bones. "
-]
-
-def execute_record():
+def execute_dictation():
     r = sr.Recognizer()
     with sr.Microphone() as input:
         print("recording is in progress...")
