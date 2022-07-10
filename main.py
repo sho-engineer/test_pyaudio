cebaca25d4012775df9ca38d1bc4c2653a858294
sho-engineer <arlonbolt@gmail.com>
2022-07-11 01:15:00 +0900

    fix fix the commit messages

--- main.py
+++ main.py
@@ -53,53 +53,56 @@ def init_record_setting():
 def record():
     # initialize the setting for recording
     init = init_record_setting()
-    stream = py_audio.open(format=init['format'], channels=init['channel'], rate=init['rate'],
+    # create an PuyAudio instance 
     py_audio = pyaudio.PyAudio()
     # open pyaudio stream with the setting defined above
+    stream = py_audio.open(format=init['format'], channels=init['channel'], rate=init['rate'],
                             input=True, frames_per_buffer=init['chunk'])
-    frames = []
     # define an empty array 
-    for i in range(0, int(init['rate'] / init['chunk'] * init['record_time'])):
+    frames = []
     # record body
+    for i in range(0, int(init['rate'] / init['chunk'] * init['record_time'])):
         data = stream.read(init['chunk'])
         frames.append(data)
-    stream.stop_stream()
     # stop streaming
+    stream.stop_stream()
     stream.close()
     py_audio.terminate()
-    output_wave_file(frames=frames, pyaudio=py_audio)
     # output as a wave file
+    output_wave_file(frames=frames, pyaudio=py_audio)
 
 def output_wave_file(frames, pyaudio):
-    init = init_record_setting()
     # initiaze the setting for recording
-    wf = wave.open(init['output_path'], 'wb')
+    init = init_record_setting()
     # open a file with writable mode
-    wf.setnchannels(init['channel'])
+    wf = wave.open(init['output_path'], 'wb')
     # set some settings 
+    wf.setnchannels(init['channel'])
     wf.setsampwidth(pyaudio.get_sample_size(init['format']))
     wf.setframerate(init['rate'])
-    wf.writeframes(b''.join(frames))
     # write data to the wave file
-    wf.close()
+    wf.writeframes(b''.join(frames))
     # end output
+    wf.close()
 
 if __name__ == '__main__':
     # execute_record()
     try:
-        for text in parse_text(TEXTS[0]):
-            input_text = execute_record()
-            lower_input_text = upper_to_lower(input_text)
-            text = upper_to_lower(text)
-            print(input_text)
-            print(text)
-            if input_text == 'stop':
-                print('Recordig stopped ...')
-                break
+        record_data = record()
+        output_wave_file(record_data[0], record_data[1])
+        # for text in parse_text(TEXTS[0]):
+        #     input_text = execute_dictation()
+        #     lower_input_text = upper_to_lower(input_text)
+        #     text = upper_to_lower(text)
+        #     print(input_text)
+        #     print(text)
+        #     if input_text == 'stop':
+        #         print('Recordig stopped ...')
+        #         break
 
-            if lower_input_text == text:
-                print("correct")
-            else:
-                print('false')
+        #     if lower_input_text == text:
+        #         print("correct")
+        #     else:
+        #         print('false')
     except Exception as e:
         print(f'error occured: {e}')
